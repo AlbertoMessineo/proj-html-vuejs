da1cf3a7c6c3cd344205fa4f84dd97585d638716
AlbertoMessineo <albevorox@gmail.com>
2023-12-19 16:00:00 +0100

    feat: colors storaged

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,18 +11,34 @@ export default {
 </script>
 
 <template>
-    <div class="header">
+    <div class="header text-center container d-flex justify-content-between">
         <img src="../assets/img/logo-2.png" alt="logo_header" id="logo-header">
         <nav>
-            <ul>
-                <li>prova</li>
-                <li>prova</li>
-                <li>prova</li>
-                <li>prova</li>
-                <li>prova</li>
-                <li>prova</li>
-                <li>prova</li>
-                <li>prova</li>
+            <ul class="d-flex">
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About us</a>
+                </li>
+                <li>
+                    <a href="">Feature</a>
+                </li>
+                <li>
+                    <a href="">Testimonials</a>
+                </li>
+                <li>
+                    <a href="">Contact US</a>
+                </li>
+                <li>
+                    <a href="">icona 1</a>
+                </li>
+                <li>
+                    <a href="">icona 2</a>
+                </li>
+                <li>
+                    <button>Get Started</button>
+                </li>
             </ul>
         </nav>
     </div>
@@ -30,10 +46,5 @@ export default {
 </template>
 
 <style scoped lang="scss">
-.header{
-    display: flex;
-    ul{
-        display: flex;
-    }
-}
+
 </style>
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -7,7 +7,20 @@ export default {
 </script>
 
 <template>
-
+<div class="container-big d-flex justify-content-between m-auto">
+  <div class="left col-6">
+    <h2>The biggest platform for your growth.</h2>
+    <p>There are many variations of passages of lorem ipsum available, but majorities have suffered alteration in some form</p>
+    <button>read more</button>
+  </div>
+  <div class="right col-6"></div>
+  
+  
+</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container-big{
+  background-color: $light-blue;
+}
+</style>
