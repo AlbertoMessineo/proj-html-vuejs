d34a7a523583fa1e615a42620b9c2f406a2a2e51
AlbertoMessineo <albevorox@gmail.com>
2023-12-19 15:33:06 +0100

    feat: imported bootsrap via CDN (to fix)

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import { createApp } from 'vue';
 import './assets/scss/style.scss';
 //file di partenza Vue
 import App from './App.vue';
+//importazione bootstrap
+// import 'bootstrap/assets/scss/bootstrap.scss';
+
 
 createApp(App).mount('#app');
